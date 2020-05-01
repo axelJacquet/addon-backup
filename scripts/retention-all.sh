a5deece1e4b1b1e2669cceb2955b3b74f192b772
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-05-01 10:42:37 -0700

    Update retention-all.sh

--- scripts/retention-all.sh
+++ scripts/retention-all.sh
@@ -12,46 +12,7 @@ touch /home/plan.json
 
 . /home/.config/swissbackup/openrc.sh
 
-usage2() {  1>&2; exit 1; }
-
-while getopts "h:d:m:y:" o; do
-    case "${o}" in
-
-        h)
-            hour=${OPTARG}
-            ;;
-        d)
-            day=${OPTARG}
-            ;;
-
-        m)
-            month=${OPTARG}
-            ;;
-
-        y)
-            year=${OPTARG}
-            ;;
-    esac
-done
-shift $((OPTIND-1))
-
-
-restic unlock
-
-if ! eval "/usr/bin/restic backup --hostname $host --tag filesystem --one-file-system /"; then
-
-         restic unlock
-fi
-
-sleep 5
-
-if ! eval "/usr/bin/restic forget --tag filesystem --keep-within "$year"y"$month"m"$day"d"$hour"h --prune"; then
-
-         restic unlock
-
-         restic rebuild-index
-
-fi
+eval "/usr/bin/restic backup --hostname $host --tag filesystem --one-file-system /"
 
 > /home/plan.json
 

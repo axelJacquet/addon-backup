1247d9e3ef78262a85e3f161ca18f69592d5d540
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-05-01 08:24:07 -0700

    Update purge.sh

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -9,27 +9,6 @@ time_limit=$(($now + 7200))
 
 . /home/.config/swissbackup/openrc.sh
 
-usage () {
-        echo "$0 --folders-to-backup <folder1>[,folder2,folder3,...]"
-        exit 1
-}
-for i in $@ ; do
-        case "${1}" in
-        "--folders-to-backup"|"-f")
-        echo $1
-                if [ -z "${2}" ] ; then
-                        echo "No parameter defining the --folder-to-backup parameter"
-                        usage
-                fi
-                FOLDER_TO_BACKUP="${2}"
-                shift
-                shift
-        ;;
-        *)
-        ;;
-        esac
-done
-
 usage2() {  1>&2; exit 1; }
 
 while getopts "s:h:d:w:m:y:" o; do
@@ -61,23 +40,22 @@ while getopts "s:h:d:w:m:y:" o; do
 done
 shift $((OPTIND-1))
 
-for p in ${FOLDERS_TO_BACKUP}"" ; do
-	while true
-		do
-			if [ $now -gt $timelimit  ]
-			then
-				echo "`date +'%Y%m%d%H%M'`: Timeout. exiting "
-							exit 1
-			elif [ `restic list locks --no-lock | wc -l` -gt 0 ]
-			then
-				echo "`date +'%Y%m%d%H%M'`: Backup waiting for lock" > /root/retention.log
-			else
-				echo "`date +'%Y%m%d%H%M'`: Deleting $p" > /root/retention.log
-	                        eval "/usr/bin/restic forget --host $host --tag $p --keep-within "$year"y"$month"m"$day"d"$hour"h --prune" > /root/retention.log
-			fi
-			sleep 60
-		done
-done
+while true
+	do
+		if [ $now -gt $timelimit  ]
+		then
+			echo "`date +'%Y%m%d%H%M'`: Timeout. exiting "
+			exit 1
+		elif [ `restic list locks --no-lock | wc -l` -gt 0 ]
+		then
+			echo "`date +'%Y%m%d%H%M'`: Backup waiting for lock" > /root/retention.log
+		else
+			echo "`date +'%Y%m%d%H%M'`: Deleting $p" > /root/retention.log
+	                eval "/usr/bin/restic forget --host $host --keep-within "$year"y"$month"m"$day"d"$hour"h --prune" > /root/retention.log
+		fi
+		sleep 60
+	done
+
 
 > /home/plan.json
 

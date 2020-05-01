54512e51fb4d99e8bae98f9e068da1f4ffe10374
axelJacquet <47757198+axelJacquet@users.noreply.github.com>
2020-05-01 06:46:26 -0700

    Update purge.sh

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -8,6 +8,59 @@ time_limit=$(($now + 7200))
 
 . /home/.config/swissbackup/openrc.sh
 
+usage () {
+        echo "$0 --folders-to-backup <folder1>[,folder2,folder3,...]"
+        exit 1
+}
+for i in $@ ; do
+        case "${1}" in
+        "--folders-to-backup"|"-f")
+        echo $1
+                if [ -z "${2}" ] ; then
+                        echo "No parameter defining the --folder-to-backup parameter"
+                        usage
+                fi
+                FOLDER_TO_BACKUP="${2}"
+                shift
+                shift
+        ;;
+        *)
+        ;;
+        esac
+done
+
+usage2() {  1>&2; exit 1; }
+
+while getopts "s:h:d:w:m:y:" o; do
+    case "${o}" in
+
+        s)
+            last_snapshot=${OPTARG}
+            ;;
+
+
+        h)
+            hour=${OPTARG}
+            ;;
+        d)
+            day=${OPTARG}
+            ;;
+        w)
+            week=${OPTARG}
+            ;;
+
+        m)
+            month=${OPTARG}
+            ;;
+
+        y)
+            year=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
 while true
         do
                 if [ $now -gt $timelimit  ]
